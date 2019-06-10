b73dac2d574a004e1aa085b722f02d6c489350fd
kashitaku <takumi.kashima47@gmail.com>
2019-06-10 15:18:15 +0900

    display.php_post.php_edit

--- display.php
+++ display.php
@@ -1,13 +1,11 @@
-CTYPE html>
+<!DOCTYPE html>
 <html lang="ja">
 <head>
 <meta charset="utf-8">
 </head>
 <body>
 <h1>表示画面</h1>
-<?php
-echo $_GET['data'];
-?>
+<?php echo $_GET['data']; ?>
 </body>
 </html>
 
